66f02964c7fccc8d5ab45224d4a89ab5f362bbb5
lixiaojin123 <20141105062l@fanydeimac13.local>
2016-07-03 09:57:13 +0800

    Initial Commit

---
+++ address bookTests/address_bookTests.swift
@@ -0,0 +1,36 @@
+//
+//  address_bookTests.swift
+//  address bookTests
+//
+//  Created by 李晓晋 on 16/7/3.
+//  Copyright © 2016年 李晓晋. All rights reserved.
+//
+
+import XCTest
+@testable import address_book
+
+class address_bookTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
