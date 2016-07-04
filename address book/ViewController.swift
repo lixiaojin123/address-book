a038eab80d4cdbf47fe59dab7ed9b8580333bba2
lixiaojin123 <20141105062l@FanydeiMac13.local>
2016-07-04 09:41:18 +0800

    编写通讯录第二次
    首先做的是储存按钮，如果数据库中有数据则直接加载，没有的话要创建一个表。如果存在数据则将数据放在表中，判断表中的数据数是否大于0，如果是的话获取
    最后一行数据显示，如果没有的话讲text中输入的数据存在数据库中。

--- address book/ViewController.swift
+++ address book/ViewController.swift
@@ -13,9 +13,14 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var address: UITextField!
+    @IBOutlet weak var email: UITextField!
     @IBAction func save(sender: AnyObject) {
          saveUser()
     }
+ 
+   
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,7 +28,7 @@ class ViewController: UIViewController {
         //获取数据库实例
         db = SQLiteDB.sharedInstance()
         //如果表还不存在则创建表（其中uid为自增主键）
-        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20),address varchar(20),email varchar(20),)")
         //如果有数据则加载
         initUser()
     }
@@ -38,7 +43,9 @@ class ViewController: UIViewController {
             let user = data[data.count - 1]
             name.text = user["uname"] as? String
             phone.text = user["mobile"] as? String
-        }
+            address.text = user["address"] as? String
+            email.text = user["email"] as? String
+                    }
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -50,8 +57,10 @@ class ViewController: UIViewController {
     func saveUser() {
         let name = self.name.text!
         let phone = self.phone .text!
+        let address = self.address.text!
+        let email = self.email.text!
         //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
-        let sql = "insert into t_user(uname,mobile) values('\(name)','\(phone)')"
+        let sql = "insert into t_user(uname,mobile,address,email) values('\(name)','\(phone)','\(address)','\(email)')"
         print("sql: \(sql)")
         //通过封装的方法执行sql
         let result = db.execute(sql)
