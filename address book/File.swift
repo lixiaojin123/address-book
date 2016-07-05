ef93699ddf67d0cd8891625f7c6a49c0cf77cbc7
lixiaojin123 <20141105062l@FanydeiMac13.local>
2016-07-05 09:13:17 +0800

    编写通讯录第三次
    其次做的是查询按钮，将写入数据库中的所有数据全部显示出来，首先还是获取数据库实例，在数据库中创建一个表，定义一个显示所有数据的函数，将从数据库中
    获取到的数据放在data中，然后数出数据的个数，将data中的数据放入常量u中，然后输出序号和数据

---
+++ address book/File.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  address book
+//
+//  Created by 李晓晋 on 16/7/3.
+//  Copyright © 2016年 李晓晋. All rights reserved.
+//
+
+import UIKit
+
+class File: UIViewController {
+    var db:SQLiteDB!
+    
+    @IBOutlet weak var x: UITextView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        
+        //获取数据库实例
+        db = SQLiteDB.sharedInstance()
+        //如果表还不存在则创建表（其中uid为自增主键）
+        db.execute("create table if not exists user(uid integer primary key,uname varchar(20),mobile varchar(20),address varchar(20),email varchar(20))")
+        initUser()
+    }
+
+    
+    
+ 
+    func initUser()
+    {
+        let data = db.query("select * from user")
+        for var i=0 ; i < data.count; i++
+        {
+            //获取最后一行数据显示
+            let u = data[i]
+            x.text! += "\(i)"
+            x.text! += "  name： "+String(u["uname"]!)+"  phone: "+String(u["mobile"]!)+"   address: "+String(u["address"]!)+"  email: "+String(u["email"]!)+"\n"
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
--- address book/ViewController.swift
+++ address book/ViewController.swift
@@ -28,16 +28,16 @@ class ViewController: UIViewController {
         //获取数据库实例
         db = SQLiteDB.sharedInstance()
         //如果表还不存在则创建表（其中uid为自增主键）
-        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20),address varchar(20),email varchar(20),)")
+        db.execute("create table if not exists user(uid integer primary key,uname varchar(20),mobile varchar(20),address varchar(20),email varchar(20))")
         //如果有数据则加载
-        initUser()
+        //initUser()
     }
 
     
     
     //从SQLite加载数据
     func initUser() {
-        let data = db.query("select * from t_user")
+        let data = db.query("select * from user")
         if data.count > 0 {
             //获取最后一行数据显示
             let user = data[data.count - 1]
@@ -60,7 +60,7 @@ class ViewController: UIViewController {
         let address = self.address.text!
         let email = self.email.text!
         //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
-        let sql = "insert into t_user(uname,mobile,address,email) values('\(name)','\(phone)','\(address)','\(email)')"
+        let sql = "insert into user(uname,mobile,address,email) values('\(name)','\(phone)','\(address)','\(email)')"
         print("sql: \(sql)")
         //通过封装的方法执行sql
         let result = db.execute(sql)
